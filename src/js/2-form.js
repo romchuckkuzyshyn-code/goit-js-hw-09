8e5926d08126aba1cb69bd8da35723044f0eb48d
Roman <romchuck.kuzyshyn@gmail.com>
2026-02-10 22:06:34 +0200

    fixed task-2 and index

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,20 +1,39 @@
 const formData = { email: '', message: '' };
-
+const STORAGE_KEY = 'feedback-form-state';
 const feedback = document.querySelector('.feedback-form');
 
-feedback.addEventListener('input', getFormData);
-const savedData = loadLS('feedback-form', {});
+const savedData = loadLS(STORAGE_KEY, {});
 formData.email = savedData.email || '';
 formData.message = savedData.message || '';
 
 feedback.elements.email.value = formData.email;
 feedback.elements.message.value = formData.message;
 
+feedback.addEventListener('input', getFormData);
+feedback.addEventListener('submit', onFormSubmit);
+
 function getFormData(event) {
   const name = event.target.name;
-  const value = event.target.value;
+  const value = event.target.value.trim();
   formData[name] = value;
-  saveLS('feedback-form', formData);
+  saveLS(STORAGE_KEY, formData);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (formData.email.length === 0 || formData.message.length === 0) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  feedback.reset();
+
+  formData.email = '';
+  formData.message = '';
 }
 
 function saveLS(key, value) {
